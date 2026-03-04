aee46cfa4a83a10a77e02b5b59ec97c8491a0504
Corvus451 <msmark.451@gmail.com>
2026-03-04 12:34:05 +0100

    client create App class

--- client/app.py
+++ client/app.py
@@ -1,30 +1,52 @@
 import asyncio
+import json
+
 from ws.wshandler import WsHandler
 from ui.ui import UI
 from utility.logger import Logger
 
 
-def msg_handler(msg: str):
-    pass
+class App:
+    def __init__(self, ui: UI, logger: Logger):
+        self.__ws_handler = WsHandler(self.msg_handler, self.close_handler, logger)
+        self.__ui = ui
+        self.__logger = logger
 
-def close_handler():
-    pass
+        self.__ui.set_input_handler(self.input_handler)
 
-def input_handler(text: str, ui: UI):
-    if text == "/quit":
-        ui.stop()
-    else:
-        ui.set_message_display(text=text)
+    def msg_handler(self, msg: str):
+        parsed: dict = json.loads(msg)
+        if parsed.get("message"):
+            self.__ui.add_message(parsed["message"])
 
+    def close_handler(self):
+        self.__ui.add_message("Connection closed.")
 
+    async def cmd_handler(self, text: str):
+        cmd = text.split(" ")
+        if cmd[0] == "/quit":
+            await self.__ws_handler.disconnect()
+            self.__ui.stop()
+        elif cmd[0] == "/connect":
+            await self.__ws_handler.connect(cmd[1])
+        elif cmd[0] == "/disconnect":
+            await self.__ws_handler.disconnect()
+
+    async def input_handler(self, text: str):
+        if text.startswith("/"):
+            await self.cmd_handler(text)
+        else:
+            self.__ui.add_message(text)
+
+    async def run(self):
+        await self.__ui.run()
 
 async def main():
     
     logger = Logger("testlog.txt")
-    # wsHandler = WsHandler(msg_handler, close_handler, logger)
-    ui = UI(input_handler)
-
-    await ui.run()
+    ui = UI()
+    app = App(ui, logger)
+    await app.run()
 
 
 if __name__ == "__main__":
