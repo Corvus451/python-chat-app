c714979dc2747ad8b46f3f75f0cb03892fd0224d
Corvus451 <msmark.451@gmail.com>
2026-03-05 18:18:45 +0100

    client improve ws handler

--- client/ws/wshandler.py
+++ client/ws/wshandler.py
@@ -10,9 +10,16 @@ class WsHandler:
         self.__on_close = on_close
         self.__logger = logger
         self.__is_connecting: bool = False
+        self.__is_disconnecting: bool = False
 
     def is_connected(self) -> bool:
         return self.__ws and self.__ws.state == websockets.State.OPEN
+    
+    # def is_connecting(self) -> bool:
+    #     return self.__ws and self.__ws.state == websockets.State.CONNECTING
+    
+    # def is_disconnecting(self) -> bool:
+    #     return self.__ws and self.__ws.state == websockets.State.CLOSING
     
     async def connect(self, address: str, nickname: str):
         if self.__is_connecting or self.is_connected():
@@ -32,24 +39,40 @@ class WsHandler:
                 self.__ws = None
 
         try:
+            self.__is_connecting = True
+
             connection = await websockets.connect(f"ws://{address}")
             self.__ws = connection
-            asyncio.create_task(receive(self.__ws))
 
             connect_msg = json.dumps({
                 "type": "join",
                 "nickname": nickname
             })
             await self.__ws.send(connect_msg)
+            successful = await self.__ws.recv()
+            self.__on_message(successful)
+            
+            asyncio.create_task(receive(self.__ws))
+
+            self.__is_connecting = False
 
             return True
+        
         except Exception as e:
+            self.__is_connecting = False
             self.__logger.log(e)
             return False
         
     async def disconnect(self):
+        if self.__is_disconnecting:
+            return
+        
+        self.__is_disconnecting = True
+
         if self.is_connected():
             await self.__ws.close()
+
+        self.__is_disconnecting = False
 
     def __create_message(self, text: str):
         return json.dumps({
